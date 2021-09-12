05c23139fbf9eb5ea2db89f3b8b3df50d61a74f3
hp0404 <hryhorii.pavlenko@gmail.com>
2021-09-13 01:17:04 +0300

    added example request body, resolves #3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+import json
 from pathlib import Path
 
 import spacy
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from starlette.responses import RedirectResponse
 
 from app.models import InputDocument, ModelResponse
@@ -14,6 +15,14 @@ root = Path(__file__).resolve().parent
 nlp = spacy.load(root / "models")
 app = FastAPI(title="MENA policies PERSON classifier", version="1.0")
 
+text = """
+Secretary of State Mike Pompeo and Defense Secretary Mark T. Esper met with 
+President Trump at the White House following the attack. The president said 
+he will make a statement Wednesday morning...""".replace(
+    "\n", ""
+)
+example_request = json.dumps({"text": text})
+
 
 @app.get("/", include_in_schema=False)
 def docs_redirect():
@@ -21,7 +30,7 @@ def docs_redirect():
 
 
 @app.post("/entities", response_model=ModelResponse, tags=["NER"])
-async def extract_entities(body: InputDocument):
+async def extract_entities(body: InputDocument = Body(..., example=example_request)):
     """Extract Named Entities from an InputDocument."""
     ner = NER(nlp=nlp, document=body)
     return ner.extract_entities()
